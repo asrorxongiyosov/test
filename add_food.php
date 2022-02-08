631e7e08d015870c0442291cd572bdcb3b4b1e75
asrorxongiyosov <asrorxongiyosov@gmail.com>
2022-02-08 10:44:16 +0500

    add Food file changed

--- add_food.php
+++ add_food.php
@@ -52,7 +52,7 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Express</title>
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
 </head>
 
